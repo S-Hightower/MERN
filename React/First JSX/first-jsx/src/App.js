1fdca8f86375317eb00952d7c2521cfb346b3384
Shawna Hightower <sdhightower6@gmail.com>
2022-01-11 12:36:25 -0700

    assignment completion

---
+++ React/First JSX/first-jsx/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (            
+    <>                
+        <h1>Hello World</h1>                
+        <h2>This is a paragraph</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Take a nap</li>
+          <li>Catch up on One Piece</li>
+        </ul>            
+    </>        
+  );
+}
+
+export default App;
