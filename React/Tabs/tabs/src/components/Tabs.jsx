34440e6e22340834f6889bff6af616d351e6c20b
Shawna Hightower <sdhightower6@gmail.com>
2022-01-12 16:14:52 -0700

    component & css updates

--- React/Tabs/tabs/src/components/Tabs.jsx
+++ React/Tabs/tabs/src/components/Tabs.jsx
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
 
+import styles from './Tabs.module.css';
+
 const Tabs = (props) => {
     const[tabs,setTabs]= useState([]);
-    const[wasClicked, setWasClicked] = useState(false);
 
-    const onClickHandler = (e, value) => {
+    const onClickHandler = (e, index) => {
         e.preventDefault();
         setTabs(index);
-        setWasClicked(true);
     };
 
     return (
-        <>
-        <div className={style.tab} onClick={e => onClickHandler(e, 1)} value={tabs}>Tab 1</div>
-        <div className={style.tab} onClick={e => onClickHandler(e, 2)} value={tabs}>Tab 2</div>
-        <div className={style.tab} onClick={e => onClickHandler(e, 3)} value={tabs}>Tab 3</div>
-        </>
+        <div className={styles.container}>
+        <div className={styles.clickers}>
+            <div className={styles.tab} onClick={e => onClickHandler(e, 1)} value={tabs}>Tab 1</div>
+            <div className={styles.tab} onClick={e => onClickHandler(e, 2)} value={tabs}>Tab 2</div>
+            <div className={styles.tab} onClick={e => onClickHandler(e, 3)} value={tabs}>Tab 3</div>
+        </div>
+        <div className={styles.content}>
+            {tabs === 1 && <p>Tab 1: 🦄🦄🦄</p>}
+            {tabs === 2 && <p>Tab 2: 🐧🐧🐧</p>}
+            {tabs === 3 && <p>Tab 3: 🐼🐼🐼</p>}
+        </div>
+        </div>
     )
 };
 
