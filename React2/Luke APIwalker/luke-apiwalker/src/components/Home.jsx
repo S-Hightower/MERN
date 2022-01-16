034b5f7d4a49ef2a1332c6e9538ad4665c62d1aa
Shawna Hightower <sdhightower6@gmail.com>
2022-01-16 12:43:27 -0700

    home form rendered

--- React2/Luke APIwalker/luke-apiwalker/src/components/Home.jsx
+++ React2/Luke APIwalker/luke-apiwalker/src/components/Home.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { useHistory } from "react-router-dom";
+
 
 const Home = (props) => {
+    const [selection, setSelection] = useState("planets");
+    const [id, setId] = useState(7);
+    const history = useHistory();
+
+    const StarWarsPull = e => {
+        e.preventDefault();
+        history.push(`/${selection}/${id}`);
+    };
+
     return(
-        <h1>Test</h1>
-    )
+        <form onSubmit={StarWarsPull}>
+            <label>Search for:</label>
+            <select onChange={e=> setSelection(e.target.value)} value={selection}>
+                <option>Planets</option>
+                <option>People</option>
+                <option>Species</option>
+            </select>
+            <label>ID:</label>
+            <input type="number" onChange={e=> setId(e.target.value)} value={id} min={1}/>
+            <button>Commence Search!</button>
+        </form>
+    );
 };
 
 export default Home;
