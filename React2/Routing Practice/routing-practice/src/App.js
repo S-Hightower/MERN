291945e209041ceb9c35158d01462106fd5e682e
Shawna Hightower <sdhightower6@gmail.com>
2022-01-16 11:16:10 -0700

    project initialized and connection confirmed

---
+++ React2/Routing Practice/routing-practice/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+
+import {BrowserRouter, Link} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Testing</h1>
+    </div>
+  );
+}
+
+export default App;
