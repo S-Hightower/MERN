5d729379c04b309a65de54565d406556c9d18e6c
Shawna Hightower <sdhightower6@gmail.com>
2022-01-16 11:24:31 -0700

    updates

--- React2/Routing Practice/routing-practice/src/App.js
+++ React2/Routing Practice/routing-practice/src/App.js
@@ -1,12 +1,39 @@
 import React from "react";
 import './App.css';
 
-import {BrowserRouter, Link} from "react-router-dom";
+import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
+
+const Home = (props) => {
+  return (
+    <h1 style={{color: "red"}}>Home Component</h1>
+  );
+}
+
+const About = (props) => {
+  return (
+    <h1 style={{color: "blue"}}>About Component</h1>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-        <h1>Testing</h1>
+      <BrowserRouter >
+        <p>
+          <Link to="/">Home</Link>
+          |
+          <Link to="/about">About</Link>
+        </p>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
