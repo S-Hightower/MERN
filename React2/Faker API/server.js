e178a197bce9f24fe1cb2b1dee1bf7e7bc037660
Shawna Hightower <sdhightower6@gmail.com>
2022-01-18 14:23:28 -0700

    assignment completion

--- React2/Faker API/server.js
+++ React2/Faker API/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const faker = require('faker');
 const app=express();
-const port = 8000;
+
+const faker = require('faker');
+
+//port to show results
+const port = 2000;
 
 class User{
     constructor() {
@@ -13,11 +16,31 @@ class User{
         this.password = faker.internet.password();
     }
 }
-
 console.log(new User());
+
+class Company{
+    constructor() {
+        this.id = faker.datatype.uuid();
+        this.name = faker.company.companyName();
+        this.street = faker.address.streetAddress();
+        this.city = faker.address.city();
+        this.state = faker.address.state();
+        this.zipCode = faker.address.zipCode();
+        this.country = faker.address.country();
+    }
+}
+console.log(new Company());
 
 app.get('/api/users/new', (req, res) => {
     res.json(new User());
 })
 
-app.listen( port, () => console.log('Listening on port: ${port}'));
+app.get('/api/companies/new', (req, res) => {
+    res.json(new Company());
+})
+
+app.get('/api/user/company', (req, res) => {
+    res.json({User: new User(), Company: new Company()});
+})
+
+app.listen( port, () => console.log(`>>> Listening on port: ${port} <<<`));
