09f803b4f3ba3a06a9147468d0b30bf67e9958ac
Shawna Hightower <sdhightower6@gmail.com>
2022-01-18 18:00:49 -0700

    updates

---
+++ MongoDB/Jokes API/server/routes/jokes.routes.js
@@ -0,0 +1,9 @@
+const JokeController = require("../controllers/joke.controller");
+
+module.exports = app => {
+    app.get("/api/jokes", JokeController.findAllJokes);
+    app.get("/api/jokes/:id", JokeController.findOneJoke);
+    app.get("/api/jokes/update/:id", JokeController.updateExistingJoke);
+    app.get("/api/jokes/new", JokeController.createJoke);
+    app.get("/api/jokes/delete/:id", JokeController.deleteJoke);
+};
